052bd94c6ab6693c0aa6189e4371a1fc269ad411
murata0531 <coless115@gmail.com>
2022-10-12 14:13:17 +0900

    送られたidをurlにつなげてjsonを呼び出す

--- public/getJson.php
+++ public/getJson.php
@@ -1,13 +1,16 @@
 <?php
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 
-        if (isset($_POST["id"]) && $_POST["id"] == 1) {
+        if (isset($_POST["id"])) {
 
-            $spotList = json_decode(file_get_contents("./js/spotList.json"), true);
-            $panoramaList = json_decode(file_get_contents("./js/panoramaList.json"), true);
+            $id = strval($_POST["id"]);
+            $url = "./json/" . $id;
+            $spotList = json_decode(file_get_contents($url . "/spotList.json"), true);
+            $panoramaList = json_decode(file_get_contents($url . "/panoramaList.json"), true);
 
             $result = array('spotList'=> $spotList, 'panoramaList' => $panoramaList);
             header("Content-type: application/json; charset=UTF-8");
             echo json_encode($result);
+            exit;
         }
     }
